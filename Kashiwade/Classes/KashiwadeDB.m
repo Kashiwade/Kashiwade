7c813556e7304e36bd059157c0c57dece6d77378
gotojo <MXI03066@gmail.com>
2015-10-15 01:14:19 +0900

    Nifty Cloudへのアクセスが多すぎるため一度Nifty Cloud連携をやめる

--- Kashiwade/Classes/KashiwadeDB.m
+++ Kashiwade/Classes/KashiwadeDB.m
@@ -7,7 +7,7 @@
 //
 
 #import "KashiwadeDB.h"
-#import "NCMB/NCMB.h"
+// #import "NCMB/NCMB.h"
 
 @interface KashiwadeDB ()
 {
@@ -29,7 +29,7 @@
 {
     self = [super init];
     if (self != nil) {
-        [NCMB setApplicationKey:@"1be9dba7dec777026b5832322db9e5882f8e1cd5ed76fb7e45848657aa6c7ad3" clientKey:@"af6b5030e091b942a843f099084821cd911e866622065d8f8c3a57c200f3dc3a"];
+        // [NCMB setApplicationKey:@"1be9dba7dec777026b5832322db9e5882f8e1cd5ed76fb7e45848657aa6c7ad3" clientKey:@"af6b5030e091b942a843f099084821cd911e866622065d8f8c3a57c200f3dc3a"];
         kashiwadeData=[[NSMutableDictionary alloc] init];
         [kashiwadeData setObject:@0 forKey:@"真鶴1"];
         [kashiwadeData setObject:@0 forKey:@"真鶴2"];
@@ -56,45 +56,45 @@
 
 - (void)getKashiwadeData
 {
-    NCMBQuery *query = [NCMBQuery queryWithClassName:@"kashiwade"];
-    for (id key in kashiwadeData) {
-        [query whereKey:@"title" equalTo:key];
-        [query findObjectsInBackgroundWithBlock:^(NSArray *posts, NSError *error) {
-            for (NCMBObject *post in posts) {
-                [kashiwadeData setObject:[post objectForKey:@"num"] forKey:key];
-                // 再取得
-                [post refresh:nil];
-            }
-        }];
-    }
+    // NCMBQuery *query = [NCMBQuery queryWithClassName:@"kashiwade"];
+    // for (id key in kashiwadeData) {
+    //     [query whereKey:@"title" equalTo:key];
+    //     [query findObjectsInBackgroundWithBlock:^(NSArray *posts, NSError *error) {
+    //         for (NCMBObject *post in posts) {
+    //             [kashiwadeData setObject:[post objectForKey:@"num"] forKey:key];
+    //             // 再取得
+    //             [post refresh:nil];
+    //         }
+    //     }];
+    // }
 }
 
 - (void)sync:(NSTimer*)timer
 {
-    NCMBQuery *query = [NCMBQuery queryWithClassName:@"kashiwade"];
-    for (id key in kashiwadeData) {
-        [query whereKey:@"title" equalTo:key];
-        [query findObjectsInBackgroundWithBlock:^(NSArray *posts, NSError *error) {
-            for (NCMBObject *post in posts) {
-                int num=[[post objectForKey:@"num"] intValue];
-                int inc= [[incValue objectForKey:key] intValue];
-                int cur=[self getNum:key];
-                if (num==cur&&inc==0) continue;
-                num+=inc;
-                [kashiwadeData setObject:[NSNumber numberWithInt:num] forKey:key];
-                [incValue setObject:@0 forKey:key];
-                [post setObject:[NSNumber numberWithInt:num] forKey:@"num"];
-                [post saveInBackgroundWithBlock:nil];
+    // NCMBQuery *query = [NCMBQuery queryWithClassName:@"kashiwade"];
+    // for (id key in kashiwadeData) {
+    //     [query whereKey:@"title" equalTo:key];
+    //     [query findObjectsInBackgroundWithBlock:^(NSArray *posts, NSError *error) {
+    //         for (NCMBObject *post in posts) {
+    //             int num=[[post objectForKey:@"num"] intValue];
+    //             int inc= [[incValue objectForKey:key] intValue];
+    //             int cur=[self getNum:key];
+    //             if (num==cur&&inc==0) continue;
+    //             num+=inc;
+    //             [kashiwadeData setObject:[NSNumber numberWithInt:num] forKey:key];
+    //             [incValue setObject:@0 forKey:key];
+    //             [post setObject:[NSNumber numberWithInt:num] forKey:@"num"];
+    //             [post saveInBackgroundWithBlock:nil];
 
-                [[NSNotificationCenter defaultCenter] postNotificationName:@"DataUpdated"
-                                                                    object:self
-                                                                  userInfo:@{@"key":key}];
+    //             [[NSNotificationCenter defaultCenter] postNotificationName:@"DataUpdated"
+    //                                                                 object:self
+    //                                                               userInfo:@{@"key":key}];
                  
-                // 再取得
-                [post refresh:nil];
-            }
-        }];
-    }
+    //             // 再取得
+    //             [post refresh:nil];
+    //         }
+    //     }];
+    // }
 }
 
 - (int)getNum:(NSString*)key
